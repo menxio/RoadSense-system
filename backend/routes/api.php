df7023f0cdc1f4978b13f18acd88ca9631ce9266
Clement <jayceerubiato@gmail.com>
2025-04-16 08:18:56 +0800

    Successful connection of backend and frontend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
-Route::get('/example-endpoint', function () {
-    return response()->json(['message' => 'This is an example endpoint']);
-});
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+Route::get('/user', function () {
+    return auth()->user();
+})->middleware('auth:sanctum');
