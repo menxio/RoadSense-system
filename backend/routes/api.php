dc5b31d25c2fdb3e3c0a7187ff69531d229c27ed
rzeath <ernesto.tacumba01@gmail.com>
2025-04-17 20:12:49 +0800

    merging with feature/admin-dashboard

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,17 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
-<<<<<<< HEAD
-Route::get('/user/dashboard', function () {
-    return response()->json(['message' => 'Hello from the backend!']);
-});
-Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
-Route::get('/user', function () {
-    return auth()->user();
-})->middleware('auth:sanctum');
-=======
 Route::post('/register', [AuthController::class, 'register']); // Register route
 Route::post('/login', [AuthController::class, 'login']); // Login route
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum'); // Logout route
@@ -26,4 +15,3 @@ Route::prefix('user')->middleware('auth:sanctum')->group(function () {
     Route::put('/{id}', [UserController::class, 'update']);
     Route::delete('/{id}', [UserController::class, 'delete']);
 });
->>>>>>> a3b412e56362bfa22b33fe252972a3c39a7f0e2b
