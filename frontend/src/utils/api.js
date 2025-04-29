42e3c8fcefc71849794877453825d3f4940653fa
menxio <jayceerubiato@gmail.com>
2025-04-29 23:53:44 +0800

    UI/UX enhancements, implementing notification

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://172.20.10.3:8000/api",
-  baseURL: "http://backend.test/api",
+  baseURL: "http://172.20.10.3:8000/api",
+  // baseURL: "http://backend.test/api",
   withCredentials: true,
 });
 
